85bf7b14f683ac16ed30f4b0ba08f4976a3732fa
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2022-11-18 11:29:56 -0800

    Added HealthCollectible script
    wrote HealthCollectible script and added collectible game object. also started working on damagables

--- RubysAdventureAksabanovic/Assets/Scripts/RubyController.cs
+++ RubysAdventureAksabanovic/Assets/Scripts/RubyController.cs
@@ -1,17 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -21,12 +29,18 @@ public class RubyController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 2.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 2.0f * vertical * Time.deltaTime;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
